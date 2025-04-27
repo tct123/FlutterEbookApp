e41aacaa3386cce41ff9b6c0aa3b79d0d3081edf
tct123 <42028373+tct123@users.noreply.github.com>
2025-04-27 04:58:36 +0200

    .

---
+++ android/app/src/main/kotlin/com/example/flutter_ebook_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_ebook_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/dev/jideguru/flutter_ebook_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.jideguru.flutter_ebook_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
